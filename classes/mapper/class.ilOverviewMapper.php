82039a2ea29c60fb895dbaf21aba32821251f04c
Martin Dinkel <hmdinkel@web.de>
2017-03-24 11:17:15 +0100

    echos raus

--- classes/mapper/class.ilOverviewMapper.php
+++ classes/mapper/class.ilOverviewMapper.php
@@ -158,7 +158,6 @@ class ilOverviewMapper
 			   ;
             
             $result= $this->db->query($query);
-            //echo("<script>console.log('PHP: q ');</script>");
              return  $result; 
                         
         }
